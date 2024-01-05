1ff6dc2f8d3fa2a2509f53e5786dbde51231bce0
fiubamillanmj <mjmillan@fi.uba.ar>
2024-01-05 14:27:52 -0300

    mend

--- source/settings/settings.py
+++ source/settings/settings.py
@@ -25,10 +25,7 @@ class Settings:
     KEY_USER_DETAILS: str
     URL_SERVICIOS_TECNO: str
     KEY_GDR_FRONT: str
-    GOOGLE_CLIENT_ID: str
-    GOOGLE_CLIENT_SECRET: str
-    REDIRECT_URI: list
-    URL_BACK: str
+
     
 load_dotenv()
 
@@ -54,11 +51,7 @@ settings = Settings(
     KEY_USER_DETAILS = os.getenv('KEY_USER_DETAILS'),
     URL_SERVICIOS_TECNO= os.getenv('URL_SERVICIOS_TECNO'),
     KEY_GDR_FRONT= os.getenv('KEY_GDR_FRONT'),
-    GOOGLE_CLIENT_ID= os.getenv('GOOGLE_CLIENT_ID'),
-    GOOGLE_CLIENT_SECRET= os.getenv('GOOGLE_CLIENT_SECRET'),
-    REDIRECT_URI= os.getenv('REDIRECT_URI'),
-    URL_BACK= os.getenv('URLBACK')
-        
+   
     )
 
 
